8f350810f45d98bc498e40e332be6b3fcff03ad7
johnclamy <johnclamy@gmail.com>
2025-04-19 09:44:19 +0100

    add facet counts to filters in the admin site

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -11,4 +11,5 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
+    show_facets = admin.ShowFacets.ALWAYS
 
