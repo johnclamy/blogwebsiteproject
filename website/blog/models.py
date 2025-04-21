9313e8e12a215af27b669dffa228e9880c7336b7
johnclamy <johnclamy@gmail.com>
2025-04-21 01:19:31 +0100

    add custom manager to retrieve all published posts

--- website/blog/models.py
+++ website/blog/models.py
@@ -3,6 +3,15 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+                .get_queryset()
+                .filter(status=Post.Status.PUBLISHED)
+        )
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -24,6 +33,8 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+    objects = models.Manager()         # the default manager
+    published = PublishedManager()     # custom manager
 
     class Meta:
         ordering = ['-publish']
