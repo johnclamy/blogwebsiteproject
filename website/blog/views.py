b804733600c13e7345f8cb7607d0e15533114cc4
johnclamy <johnclamy@gmail.com>
2025-05-02 11:19:57 +0100

    replace post_list func with PostListView class

--- website/blog/views.py
+++ website/blog/views.py
@@ -4,25 +4,25 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator # type:
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 8)      # Pagination with eight posts per page
-    page_number = request.GET.get('page', 1)
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 8)      # Pagination with eight posts per page
+#     page_number = request.GET.get('page', 1)
 
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
 
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts}
+#     )
 
 
 def post_detail(request, year, month, day, post):
